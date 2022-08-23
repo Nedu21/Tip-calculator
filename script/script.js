1ce5f4eec8ca1e6d8a710370defae434f17fd3cb
Nedu21 <michaelanayo64@gmail.com>
2022-08-23 16:24:16 +0100

    Update 1

--- script/script.js
+++ script/script.js
@@ -5,9 +5,6 @@ const noOfPeople = document.getElementById("persons");
 const tip = document.querySelector(".tipcontainer__inputs--tips");
 const customTip = document.querySelector(".custom");
 
-const errorMsg = document.querySelector(".errormsg");
-const poeple = document.getElementById("persons");
-
 const tipAmount = document.getElementById("tip__amount");
 const tipTotalAmount = document.getElementById("tip__total--Amount");
 const resetBtn = document.querySelector(
@@ -18,16 +15,3 @@ const resetBtn = document.querySelector(
 if (tipTotalAmount.innerText !== "$0.00") {
   resetBtn.classList.add("reset__active");
 }
-
-noOfPeople.addEventListener("input", function (e) {
-  const noOfPeopleValue = +e.target.value;
-  console.log(noOfPeopleValue);
-
-  if (noOfPeopleValue === 0) {
-    errorMsg.style.visibility = "initial";
-    poeple.style.outline = "2px solid #c65139";
-  } else if (noOfPeopleValue > 0) {
-    errorMsg.style.visibility = "hidden";
-    poeple.style.outline = "2px solid #26c0ab";
-  }
-});
